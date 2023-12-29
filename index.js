efeba9e372a1f19d49b698e3d1d34e4cb29ff736
Ramragul <iotprograms@gmail.com>
2023-12-29 18:51:31 +0400

    Multiple Changes to the Backend

--- index.js
+++ index.js
@@ -2,10 +2,30 @@
 
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
+var mysql = require('mysql');
+const path = require('path')  
 
 const PORT = process.env.PORT || 3002;
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+function dbConnection () {
+  var connection = mysql.createConnection({
+    host     : "smartdisplay.cj0ybsa00pzb.ap-northeast-1.rds.amazonaws.com",
+    user     : "admin",
+    password : "root1234",
+    port     : "3306",
+    database : "smartdisplay"
+  });
+  return connection;
+}
+
+
+
 
 
 
@@ -29,11 +49,242 @@ app.use(function (req, res, next) {
 });
 app.options('*', cors());
 
-app.get("/api", (req, res) => {
+app.get("/api/test", (req, res) => {
     res.json({ message: "Hello from server!" });
   });
+
+  app.get('/db',(req,res) => {
+
+   var connection = dbConnection();
+   
+    connection.connect();
+
+    
+      console.log('Connected to database.' +connection);
   
+      let query = 'SELECT * FROM GB_Partner';
+      connection.query (query,(err,data) => {
+        if(err) throw err;
+        console.log(data)
+        res.json({data})
+      })
+      connection.end();
+      console.log("Connection Ended ")
+    });
+
+  // Icare Login Database 
+
+  app.get('/api/login',(req,res) => {
+
+    var connection = dbConnection();
+    //var userId =  req.params['id'] 
+    const userId = req.header('userId')
+    const password = req.header('password')
+    var verified = false;
+
+    //console.log("UserId Value is :" +userId)
+    //console.log("Password Value is :" +password)
+    
+     connection.connect();
+ 
+     
+       console.log('Connected to database.' +connection);
+   
+       let query = "SELECT * FROM Login WHERE User_Id = '"+userId+"'";
+       connection.query (query,(err,data) => {
+         if(err) throw err;
+
+         if(data.length>0)
+         {
+        console.log("Password from DB" +data[0].User_Password)
+        verified = (password == data[0].User_Password) ? true : false
+         }
+        res.json({"login_verified": verified})
+        //res.json({data})
+       })
+       
+       connection.end();
+       console.log("Connection Ended ")
+     });
+
+     app.post('/api/login',(req,res) => {
+
+      var connection = dbConnection();
+      //var userId =  req.params['id'] 
+      const userId = req.body.email
+      const password = req.body.password
+      var verified = false;
   
+      console.log("UserId Value is :" +userId)
+      //console.log("Password Value is :" +password)
+      
+       connection.connect();
+   
+       
+         console.log('Connected to database.' +connection);
+     
+         let query = "SELECT * FROM Login WHERE User_Id = '"+userId+"'";
+         connection.query (query,(err,data) => {
+           if(err) throw err;
+  
+           if(data.length>0)
+           {
+          console.log("Password from DB" +data[0].User_Password)
+          verified = (password == data[0].User_Password) ? true : false
+           }
+          res.json({"login_verified": verified})
+          //res.json({data})
+         })
+         
+         connection.end();
+         console.log("Connection Ended ")
+       });
+
+      app.get('/api/experts',(req,res) => {
+
+        var connection = dbConnection();
+        //var userId =  req.params['id'] 
+        //const userId = req.body.email
+        //const password = req.body.password
+        //var verified = false;
+    
+        //console.log("UserId Value is :" +userId)
+        //console.log("Password Value is :" +password)
+        
+         connection.connect();
+     
+         
+           console.log('Connected to database.' +connection);
+       
+           let query = "SELECT * FROM IC_ExpertProfile";
+           connection.query (query,(err,data) => {
+            if(err) throw err;
+            
+            res.json({data})
+           })
+           
+           connection.end();
+           console.log("Connection Ended ")
+         });
+  
+  app.post("/userQuery", (req, res) => {
+   // res.json(req)
+   console.log("Received Data")
+   console.log(req.body.fullName)
+      //Incoming Data Extraction
+
+      var FullName = req.body.fullName;
+      var MobileNumber = req.body.mobileNumber;
+      var City = req.body.city;
+      var Email = req.body.email;
+      var Message = req.body.message;
+      var State = "Tamil Nadu";
+      var Country = "India";
+      var QueryDate = new Date().toISOString().split('T')[0];
+      var QueryStatus = "O"
+
+      console.log(QueryDate);
+   
+      // DataBase Insert Logic
+   
+      var con = dbConnection();
+      con.connect();
+      console.log('Connected to database.' +con);
+   
+      
+     
+      var sql = "INSERT INTO GB_UserQuery (FullName, MobileNumber, Email, City, State, Country, Message, QueryDate, QueryStatus) VALUES ('"+FullName+"', '"+MobileNumber+"','"+Email+"','"+City+"','"+State+"','"+Country+"','"+Message+"','"+QueryDate+"','"+QueryStatus+"')";  
+      con.query(sql, function (err, result) {  
+      if (err) throw err;  
+      console.log("1 record inserted");  
+      });  
+      con.end();
+      res.json({ message: "Data Received Successfully" });
+   });
+  app.post("/api/experts", (req, res) => {
+   // res.json(req)
+   console.log("Received Data")
+   console.log(req.body.fullName)
+      //Incoming Data Extraction
+
+      var ExpertCategory = req.body.category.key;
+      console.log(req.body.category.key)
+      var FullName = req.body.name;
+      var Age = req.body.age;
+      var DOB = req.body.dob;
+      var AadharNumber = req.body.aadharNumber;
+      var ExpertImage = req.body.images[0];
+      var AadharImage = "www.AadharImage.com";
+      var City = req.body.city;
+      var State = "Tamil Nadu";
+      var Country = "India";
+      var Address = req.body.address;
+      var Pincode = req.body.pincode;
+      var MobileNumber = req.body.mobileNumber
+      var Email = req.body.email;
+      var Message = (req.body.message) ? req.body.message : "";
+      var Availability = (req.body.availability) ? req.body.availability : "NotAvailable";
+      var AccountStatus = (req.body.accountStatus) ? req.body.accountStatus : "Pending";
+      var EnrollmentDate = new Date().toISOString().split('T')[0];
+      var LastModifiedDate = new Date().toISOString().split('T')[0];
+
+      //console.log(QueryDate);
+   
+      // DataBase Insert Logic
+   
+      var con = dbConnection();
+      con.connect();
+      console.log('Connected to database.' +con);
+   
+      
+     
+      var sql = "INSERT INTO IC_ExpertProfile (ExpertCategory,FullName, Age, DOB, AadharNumber, ExpertImage, AadharImage, City, State, Country, Address, Pincode, MobileNumber, Email, Message, Availability, AccountStatus, EnrollmentDate, LastModifiedDate) VALUES ('"+ExpertCategory+"','"+FullName+"', '"+Age+"','"+DOB+"','"+AadharNumber+"','"+ExpertImage+"','"+AadharImage+"','"+City+"','"+State+"','"+Country+"','"+Address+"','"+Pincode+"','"+MobileNumber+"','"+Email+"','"+Message+"','"+Availability+"','"+AccountStatus+"','"+EnrollmentDate+"','"+LastModifiedDate+"')";  
+      con.query(sql, function (err, result) {  
+      if (err) throw err;  
+      console.log("1 record inserted");  
+      });  
+      con.end();
+      res.json({ message: "Data Received Successfully" });
+   });
+
+   app.post("/api/admee/addPartners", (req, res) => {
+    // res.json(req)
+    console.log("Received Data")
+    console.log(req.body.fullName)
+       //Incoming Data Extraction
+ 
+       var FullName = req.body.fullName;
+       var MobileNumber = req.body.mobileNumber;
+       var City = req.body.city;
+       var Email = req.body.email;
+       var Remarks = req.body.message;
+       var State = "Tamil Nadu";
+       var Country = "India";
+       var Address = req.body.address;
+       var Pincode = req.body.pincode;
+       var EnrollmentDate = new Date().toISOString().split('T')[0];
+       var VehicleType = req.body.vehicleType;
+       var VehicleNumber = req.body.vehicleNumber;
+       var ExpectedAmount = req.body.expectedAmount;
+      
+    
+       // DataBase Insert Logic
+    
+       var con = dbConnection();
+       con.connect();
+       console.log('Connected to database.' +con);
+    
+       
+      
+       var sql = "INSERT INTO Admee_Partners (FullName, MobileNumber, Email, City, State, Country, Address, Pincode, Remarks, EnrollmentDate, VehicleType, VehicleNumber, ExpectedAmount) VALUES ('"+FullName+"', '"+MobileNumber+"','"+Email+"','"+City+"','"+State+"','"+Country+"','"+Address+"','"+Pincode+"','"+Remarks+"','"+EnrollmentDate+"','"+VehicleType+"','"+VehicleNumber+"','"+ExpectedAmount+"')";  
+       con.query(sql, function (err, result) {  
+       if (err) throw err;  
+       console.log("1 record inserted");  
+       });  
+       con.end();
+       res.json({ message: "Data Received Successfully" });
+    });
+
 
 
 app.listen(PORT, () => {
