9bf1446c95afaab273f171360a9a091d6889856e
Ramragul <iotprograms@gmail.com>
2024-07-17 13:37:46 +0530

    Order Api Logic bug fix order_date

--- index.js
+++ index.js
@@ -959,7 +959,7 @@ app.get('/api/cc/categories',(req,res) => {
 
             const deliveryId = deliveryResult.insertId;
             var orderDate = moment().format('YYYY-MM-DD HH:mm:ss');
-            var orderDate = new Date().toLocaleString('en-GB').replace(',', '').replace(/\/(\d{1})\//g, '-0$1-').replace(/\/(\d{2})\//g, '-$1-');
+            //var orderDate = new Date().toLocaleString('en-GB').replace(',', '').replace(/\/(\d{1})\//g, '-0$1-').replace(/\/(\d{2})\//g, '-$1-');
 
 
             // Insert order
