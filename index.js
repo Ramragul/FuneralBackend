d2b8ac4b4bff27ac7376fe0675503da4b1c5b510
Ramragul <iotprograms@gmail.com>
2024-09-09 13:51:07 +0530

    email transporter reusable logic bug fix at tailoring order api

--- index.js
+++ index.js
@@ -1342,7 +1342,7 @@ app.get('/api/cc/user/orders', async (req, res) => {
     //   },
     // });
 
-    const transporter = mailConfig();
+    
 
 
   
@@ -1482,8 +1482,6 @@ app.post('/api/cc/tailoringOrder', async (req, res) => {
     res.status(500).json({ error: 'DB Connection Error' });
   }
 
-  const transporter = mailConfig();
-
   con.beginTransaction((err) => {
       if (err) {
           return res.status(500).json({ error: err.message });
@@ -1538,9 +1536,6 @@ app.post('/api/cc/tailoringOrder', async (req, res) => {
                       con.end();
                   });
               }
-              console.log("Order Result :" +JSON.stringify(orderResult))
-             // const orderId = orderResult.order_id;
-              //console.log("Order Id is :" +order_id)
 
               con.commit((err) => {
                   if (err) {
@@ -1548,48 +1543,12 @@ app.post('/api/cc/tailoringOrder', async (req, res) => {
                           res.status(500).json({ error: err.message });
                       });
                   }
-                  // sendTailoringOrderEmail(email, name);
                   res.status(201).json({ message: 'Tailoring order placed successfully' });
                   con.end();
               });
           });
       });
   });
-
-  // Function to send registration email
-  // const sendTailoringOrderEmail = (userEmail, userName) => {
-  //   // Set the correct path to the HTML template
-  //   const templatePath = path.join(__dirname, 'emailTemplates', 'registrationEmailTemplate.html');
-  
-  //   // Read the HTML template file
-  //   fs.readFile(templatePath, 'utf-8', (err, htmlTemplate) => {
-  //     if (err) {
-  //       console.error('Error reading the email template file:', err);
-  //       return;
-  //     }
-  
-  //     // Replace {{userName}} with the actual user's name
-  //     const emailHtml = htmlTemplate.replace('{{userName}}', userName);
-  
-  //     // Define email options
-  //     const mailOptions = {
-  //       from: '"Cotton Candy Support" <support@cottoncandy.co.in>',
-  //       to: userEmail,
-  //       subject: 'Welcome to Cotton Candy!',
-  //       html: emailHtml,
-  //     };
-  
-  //     // Send the email
-  //     transporter.sendMail(mailOptions, (error, info) => {
-  //       if (error) {
-  //         res.status(500).json({ message: 'Failure in Email Delivery ' +error });
-  //       } else {
-  //         res.status(201).json({ message: 'Tailoring order placed successfully ' +info.response });
-  //       }
-  //     });
-  //   });
-  // };
-
 });
 
 
