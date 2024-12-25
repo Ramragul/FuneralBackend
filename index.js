f5013bed6295377c009d79a0ea6a346b96f46fbd
Ramragul <iotprograms@gmail.com>
2024-12-25 14:46:07 +0000

    Update test and map test candidates api creation and bug fixes

--- index.js
+++ index.js
@@ -3058,6 +3058,8 @@ app.post("/test/update", upload.single("file"), async (req, res) => {
   // const { testID, testName, testCategory, testDescription, testTimings, testValidity, testStudents, createdBy } = req.body; // Getting test details and createdBy from request body
   const { id, name, category, description, timings, validity, users, created_by } = req.body; // Getting test details and createdBy from request body
 
+  console.log("Id received form Test Details Page :" +id + "Name :" +name)
+  console.log("Data Received from Test Details Page : " +JSON.stringify(req.body))
   if (!id || !created_by) {
     return res.status(400).send({ message: "TestID and createdBy are required" });
   }
