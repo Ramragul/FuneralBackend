0da1f086a57a5f98c0382ac6fb185ce7caa0b461
Ramragul <iotprograms@gmail.com>
2024-12-25 21:12:02 +0000

    Test Details Update Api bug fix

--- index.js
+++ index.js
@@ -3053,7 +3053,7 @@ app.post("/test/update", upload.single("file"), async (req, res) => {
    const { testID, testName, testCategory, testDescription, testTimings, testValidity, testStudents, createdBy } = req.body; // Getting test details and createdBy from request body
   //const { id, name, category, description, timings, validity, users, created_by } = req.body; // Getting test details and createdBy from request body
 
-  console.log("Id received form Test Details Page :" +testID + "Name :" +testName)
+  console.log("Id received form Test Details Page :" +testID + "name :" +testName + "Created By" + createdBy );
   //console.log("Data Received from Test Details Page : " +JSON.stringify(req.body))
   // if (!id || !created_by) {
   //   return res.status(400).send({ message: "TestID and createdBy are required" });
