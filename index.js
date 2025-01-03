dcde81c42cd92db9c47d4feeda877fccf44b9581
Ramragul <iotprograms@gmail.com>
2025-01-03 00:57:28 +0000

    Html conversion lib and logic additon in the latex conversion logic

--- index.js
+++ index.js
@@ -3122,7 +3122,7 @@ app.post("/test/upload", upload.single("file"), async (req, res) => {
       // console.log(processedQuestionText);
 
       const processedQuestionText = subject === "maths"
-      ? processMathQuestion(question_text) // Only return the MathML or rendered HTML Ramco
+      ? extractMathSymbols(processMathQuestion(question_text)) // Only return the MathML or rendered HTML 
       : question_text;
 
     console.log(processedQuestionText);
