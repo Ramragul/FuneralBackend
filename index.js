56d1f08a6a98c58cc9bb1dcee983739dd91a8eea
Ramragul <iotprograms@gmail.com>
2025-02-10 20:27:57 +0530

    order_id Addition

--- index.js
+++ index.js
@@ -1730,7 +1730,7 @@ app.post('/api/cc/register', async (req, res) => {
                                     res.status(500).json({ error: err.message });
                                 });
                             }
-                            res.status(201).json({ message: 'Order placed successfully' , order_id : orderId});
+                            res.status(201).json({ order_id: 'orderId'});
                         });
                     })
                     .catch((err) => {
