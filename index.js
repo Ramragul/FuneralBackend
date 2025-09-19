afec34331d4f614283ff246b54181adae62082e9
Ramragul <iotprograms@gmail.com>
2025-09-20 02:01:39 +0530

    Get Funeral Ground API Fix, Pool Addition

--- index.js
+++ index.js
@@ -66,17 +66,17 @@ const storage = multer.diskStorage({
 // DB Connection Pool 
 
 
+
+
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit: 10, // adjust as needed
+  connectionLimit: 10,
   queueLimit: 0,
 });
-
-module.exports = pool.promise(); // promise-based API
 
 
 // Helper function to parse LaTeX or handle plain text - Maths
